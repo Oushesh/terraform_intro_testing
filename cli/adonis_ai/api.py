728992e9112eb362bfc157c6cb8c725aefb16d69
oushesh <oushesh@gmail.com>
2024-01-15 15:24:45 +0400

    added a cli

---
+++ cli/adonis_ai/api.py
@@ -0,0 +1,22 @@
+## All the functions calling the endpoints from the service of my company here
+
+import os
+import shutil
+import requests
+
+# Models
+def list_models():
+    pass
+
+def get_model_summary(model_name:str):
+    pass
+
+def delete_model(model_name:str):
+    pass
+
+def download_model(model_name:str,model_format:str):
+    pass
+
+# Engines
+def train_classifier(model_name:str,labels:list, model_selector:str,hf_dataset=None):
+    pass
---
+++ cli/adonis_ai_README.md
@@ -0,0 +1,24 @@
+# Adonis_ai AI command-line client
+
+## Installation
+
+## Token Authentication
+
+## Usage
+   Quickly get started with the following examples:
+   
+## Model
+   List all models:
+   
+   * cli command:
+
+    $ adonis_ai model list
+
+
+   * python command:
+    from adonis_ai.api import get_model_summary
+    print (get_model_summary(model_name))
+
+   Delete a model by name:
+   
+   * cli command:
