2f287ac8da12469a9b4abaeea03690d286cdac4c
oushesh <oushesh@gmail.com>
2024-01-16 18:27:28 +0400

    updated: main and variables.tf

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,24 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "us-west-3"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    //Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
 
