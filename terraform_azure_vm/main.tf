92238f249be14fb4e6688eeab60151b256cfe26a
oushesh <oushesh@gmail.com>
2024-02-03 12:06:03 +0400

    updated: main.tf

--- terraform_azure_vm/main.tf
+++ terraform_azure_vm/main.tf
@@ -79,7 +79,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "account1"
+  name                     = "llmrust01"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
@@ -89,7 +89,7 @@ resource "azurerm_storage_account" "my_storage_account" {
 
 # Create virtual machine
 resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "azure_windows_virtual_machine_1"
+  name                  = "azurevirtualllm"
   admin_username        = "azureuser"
   admin_password        = random_password.password.result
   location              = azurerm_resource_group.rg.location
